9908d515e464b97b5784aeb68d4f801f71d6c08b
Eloha <elohavergararo@gmail.com>
2025-02-25 20:25:24 +0100

    Implementación de la función listado de los productos

---
+++ src/funciones/Listado.java
@@ -0,0 +1,12 @@
+package funciones;
+import funciones.Alta;
+import producto.Producto;
+
+import java.util.HashMap;
+import java.util.UUID;
+
+public class Listado {
+    public void mostrarListado() {
+        System.out.println(Alta.listaProductos);
+    }
+}
