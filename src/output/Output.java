c1bb300ac2f7e939f70986326c8ae0b1a51268b8
Eloha <elohavergararo@gmail.com>
2025-02-25 20:01:24 +0100

    Subir archivo output para la salida de datos de la app

---
+++ src/output/Output.java
@@ -0,0 +1,17 @@
+package output;
+
+
+
+public class Output {
+    public static void mostrarMenu(){
+        System.out.println("MENU APP");
+        System.out.println("--------");
+        System.out.println("1. Alta");
+        System.out.println("2. Venta");
+        System.out.println("3. Compra");
+        System.out.println("4. Salir");
+    }
+    public static void  mostrarTexto(String mensaje){
+        System.out.println(mensaje);
+    }
+}
